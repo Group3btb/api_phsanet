16c861a839e0d929e437185437b8e3dfa441cf18
chetra <benchetra@gmail.com>
2016-08-28 15:28:34 +0700

    user update

--- src/main/java/phsanet/controllers/UserController.java
+++ src/main/java/phsanet/controllers/UserController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
